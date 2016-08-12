bc38fb30e3df47350b3189442ba74fcf2da9b42a
Patrick Cura <patrick@storbie.com>
2016-08-12 12:14:44 +1200

    sample code

---
+++ Software/ContactsApi2/ContactsApi2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContactsApi2.WebApiApplication" Language="C#" %>
---
+++ Software/ContactsApi2/ContactsApi2/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactsApi2.Models
+{
+    public class Product
+    {
+        public string id { get; set; }
+        /// <summary>
+        /// Name of the product
+        /// </summary>
+        public string name { get; set; }
+        public decimal price { get; set; }
+    }
+}
