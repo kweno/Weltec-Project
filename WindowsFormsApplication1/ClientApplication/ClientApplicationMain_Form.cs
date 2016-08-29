5f135cfa5b2a8ff3b610b5f129a8c6d1acf79955
patrickcura1989 <patrick_cura1989@yahoo.com>
2016-08-29 17:11:25 +1200

    updated hardik code

--- WindowsFormsApplication1/ClientApplication/ClientApplicationMain_Form.cs
+++ WindowsFormsApplication1/ClientApplication/ClientApplicationMain_Form.cs
@@ -54,7 +54,7 @@ namespace ClientApplication
         public ClientApplicationMain_Form()
         {
             InitializeComponent();
-            populateServerDropdown();
+            //populateServerDropdown();
 
             Server_ComboBox.TextChanged += new System.EventHandler(Server_ComboBox_TextChanged);
 
@@ -102,7 +102,7 @@ namespace ClientApplication
             //"Initial Catalog=test;" +
             //"User id=test;" +
             //"Password=test;";
-
+            /*
             // http://stackoverflow.com/questions/18654157/how-to-make-sql-query-result-to-xml-file
             //sql = "SELECT * FROM master.sys.all_parameters";
             sql = "SELECT * FROM master.sys.database_files";
@@ -124,28 +124,25 @@ namespace ClientApplication
             {
                 MessageBox.Show("Can not open connection ! ");
             }
-            /*
-            sql = "USE [MASTER]"
-                    + "GO"
-                    + "DECLARE @ExpressionToSearch VARCHAR(200)"
-                    + "DECLARE  @ExpressionToFind VARCHAR(200)"
-                    + "-- Command will create the temporary table in tempdb"
-                    + "CREATE TABLE [dbo].[#TmpErrorLog]"
-                    + "([LogDate] DATETIME NULL,"
-                    + " [ProcessInfo] VARCHAR(20) NULL,"
-                    + " [Text] VARCHAR(MAX) NULL ) ;"
-                    + "-- Command will insert the errorlog data into temporary table"
-                    + "INSERT INTO #TmpErrorLog ([LogDate], [ProcessInfo], [Text])"
-                    + "EXEC [master].[dbo].[xp_readerrorlog] 0, 1, N'Server is listening on';"
-                    + "-- retrieves the data from temporary table"
-                    + "SET @ExpressionToFind = '1433'"
-                    + "SELECT @ExpressionToSearch = [Text] FROM #TmpErrorLog where text like '%ipv4%' and ProcessInfo = 'Server'"
-                    + "Print @ExpressionToSearch"
-                    + "IF @ExpressionToSearch LIKE '%' + @ExpressionToFind + '%'"
-                    + "    PRINT 'Yes, 1433 port is using by SQL Server'"
-                    + "ELSE"
-                    + "    PRINT 'SQL Server doesn''t use default port'";
+            */
+
+            sql =     "DECLARE @ExpressionToSearch VARCHAR(200)" + Environment.NewLine
+                    + "DECLARE  @ExpressionToFind VARCHAR(200)" + Environment.NewLine
+                    + "CREATE TABLE [dbo].[#TmpErrorLog] ([LogDate] DATETIME NULL, [ProcessInfo] VARCHAR(20) NULL, [Text] VARCHAR(MAX) NULL );" + Environment.NewLine
+                    + "CREATE TABLE [dbo].[#TmpResults] ([Text] VARCHAR(MAX) NULL );" + Environment.NewLine
+                    + "INSERT INTO #TmpErrorLog ([LogDate], [ProcessInfo], [Text]) EXEC [master].[dbo].[xp_readerrorlog] 0, 1, N'Server is listening on';" + Environment.NewLine
+                    + "SET @ExpressionToFind = '1433' " + Environment.NewLine
+                    + "SELECT @ExpressionToSearch = [Text] FROM #TmpErrorLog where text like '%any%' and text like '%<ipv4>%' and text like '%1433%' and ProcessInfo = 'Server'" + Environment.NewLine
+                    + "IF @ExpressionToSearch LIKE '%' + @ExpressionToFind + '%'" + Environment.NewLine
+                    + "    INSERT INTO #TmpResults VALUES ('Yes, 1433 port is using by SQL Server');" + Environment.NewLine
+                    + "ELSE" + Environment.NewLine
+                    + "    INSERT INTO #TmpResults VALUES ('SQL Server doesn''t use default port')"
+                    + "SELECT * FROM #TmpResults;";
+
             connection = new SqlConnection(connectionString);
+            // http://stackoverflow.com/questions/1880471/capture-stored-procedure-print-output-in-net
+            // connection.InfoMessage += new SqlInfoMessageEventHandler(myConnection_InfoMessage);
+
             try
             {
                 connection.Open();
@@ -153,7 +150,7 @@ namespace ClientApplication
                 dataReader = command.ExecuteReader();
                 while (dataReader.Read())
                 {
-                    //MessageBox.Show(dataReader.GetValue(0) + " - " + dataReader.GetValue(1));
+                    MessageBox.Show(dataReader.GetValue(0)+"");
                 }
                 dataReader.Close();
                 command.Dispose();
@@ -163,7 +160,7 @@ namespace ClientApplication
             {
                 MessageBox.Show("Can not open connection ! ");
             }
-            */
+            
             //MessageBox.Show("Current Working Directory: " + Directory.GetCurrentDirectory());
 
             //using (XmlWriter writer = XmlWriter.Create("SQLServer.xml"))
@@ -197,6 +194,12 @@ namespace ClientApplication
                @"Encrypted_SQLServer.xml");
         }
 
+        /*
+        void myConnection_InfoMessage(object sender, SqlInfoMessageEventArgs e)
+        {
+            MessageBox.Show(e.Message);
+        }
+        */
 
         private void populateServerDropdown()
         {
